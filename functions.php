3b7f151c519d54f9eebf466e85ae07d64d28ac60
Guillaume Corré <gui.corre@gmail.com>
2019-06-19 13:59:27 +0200

    Spaces

--- functions.php
+++ functions.php
@@ -1,79 +1,86 @@
 <?php 
 
-//Load includes
+// Load includes
+
 require_once get_template_directory() . '/includes/styles.php';
 require_once get_template_directory() . '/includes/scripts.php';
 require_once get_template_directory() . '/includes/templating.php';
 
-//ACF Option page
+// ACF Option page
+
 if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page('Options du thème');
+  acf_add_options_page('Options du thème');
 }
 
-//Strip tags and echo field function
+// Strip tags and echo field function
+
 function strip_the_field($field, $tags) {
   echo strip_tags($field, $tags);
 }
 
+// Polylang
 
-//Polylang
 function my_pll_register_string(string $string) {
   pll_register_string($string, $string, "Theme: Gatling");
 }
 
 // Shortcut to get_template_directory_uri
+
 function add_img_html($file) {
-    $path = get_template_directory_uri() . '/assets/img/' . $file;
-    echo $path;
+  $path = get_template_directory_uri() . '/assets/img/' . $file;
+  echo $path;
 }
 
-//Custom post type USER STORIES
+// Custom post type USER STORIES
+
  $labels = array(
-	'name'              => 'User stories',
-	'singular_name'     => 'User story',
-	'all_items'         => 'All stories',
-	'view_item'         => 'View story',
+  'name'              => 'User stories',
+  'singular_name'     => 'User story',
+  'all_items'         => 'All stories',
+  'view_item'         => 'View story',
 );
 $args = array(
-	'labels'        => $labels,
-	'public'        => true,
-	'has_archive'   => true,
-	'menu_position' => 5,
-	'supports'			=> ['title', 'editor', 'thumbnail'],
+  'labels'        => $labels,
+  'public'        => true,
+  'has_archive'   => true,
+  'menu_position' => 5,
+  'supports'      => ['title', 'editor', 'thumbnail'],
 );
 
 register_post_type('user-stories', $args);
 
-//Post thumbnail
+// Post thumbnail
+
 add_theme_support( 'post-thumbnails' );
 
+// Read more link disable scroll
 
-//read more link disable scroll
 function remove_more_link_scroll( $link ) {
-	$link = preg_replace( '|#more-[0-9]+|', '', $link );
-	return $link;
+  $link = preg_replace( '|#more-[0-9]+|', '', $link );
+  return $link;
 }
+
 add_filter( 'the_content_more_link', 'remove_more_link_scroll' );
 
 //Get WP Menu
 function my_get_menu($location){
-	$menu_locations = get_nav_menu_locations();
-	$hero_menu = $menu_locations[$location];
-	$menu_items = wp_get_nav_menu_items( $hero_menu );
-	
-	$menu_ordered = array();
-	
-	foreach ($menu_items as $item){
-		if ($item->menu_item_parent == 0){
-			$menu_ordered[$item->ID] = $item;
-			$menu_ordered[$item->ID]->children = array();
-		}
-		else {
-			$menu_ordered[$item->menu_item_parent]->children[] = $item;
-		}
-	}
-	
-	return $menu_ordered;
+  $menu_locations = get_nav_menu_locations();
+  $hero_menu = $menu_locations[$location];
+  $menu_items = wp_get_nav_menu_items( $hero_menu );
+  
+  $menu_ordered = array();
+  
+  foreach ($menu_items as $item){
+    if ($item->menu_item_parent == 0){
+      $menu_ordered[$item->ID] = $item;
+      $menu_ordered[$item->ID]->children = array();
+    }
+    else {
+      $menu_ordered[$item->menu_item_parent]->children[] = $item;
+    }
+  }
+  
+  return $menu_ordered;
 }
 
 // Remove admin bar inline css
@@ -87,15 +94,13 @@ add_action('get_header', 'wl_get_header');
 // Init WP site
 
 function init_site() {
-	register_nav_menus(
-		array(
+  register_nav_menus(
+    array(
       'main-nav' => "Main menu",
       'footer-1' => "Footer menu 1",
       'footer-2' => "Footer menu 2"
-		)
-	);
+    )
+  );
 }
 
 add_action('init', 'init_site');
-
-?>
