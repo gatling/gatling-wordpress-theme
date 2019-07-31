3b08792c4c4c5a4a4deabd95118d06dfb7a601c7
Guillaume Corré <gui.corre@gmail.com>
2019-07-31 16:35:30 +0200

    Use a plugin instead of embedding prism.js inside the theme

--- includes/scripts.php
+++ includes/scripts.php
@@ -17,10 +17,6 @@ function add_js_scripts() {
     wp_register_script( 'slick', get_template_directory_uri().'/assets/js/slick.min.js?' . filemtime(get_template_directory() . '/assets/js/slick.min.js'), array('jquery'), '1.8.1', true );
     wp_enqueue_script('slick');
 
-    //prism
-    wp_register_script( 'prism', get_template_directory_uri().'/assets/js/prism.min.js?' . filemtime(get_template_directory() . '/assets/js/prism.min.js'), '1.0', true );
-    wp_enqueue_script('prism');
-
     //main JS
     wp_register_script( 'main', get_template_directory_uri().'/assets/js/main.min.js?' . filemtime(get_template_directory() . '/assets/js/main.min.js'), array('jquery'), '1.0', true );
     wp_enqueue_script('main');
@@ -28,6 +24,3 @@ function add_js_scripts() {
 }
 
 add_action('wp_enqueue_scripts', 'add_js_scripts');
-
-
-?>
--- includes/styles.php
+++ includes/styles.php
@@ -11,13 +11,8 @@ function add_styles() {
     wp_register_style( 'slick-theme', get_template_directory_uri() .'/assets/css/vendor/slick-theme.css', array(), '20190423');
     wp_enqueue_style( 'slick-theme' );
 
-    wp_register_style( 'prism', get_template_directory_uri() .'/assets/css/vendor/prism-okaidia.css', array(), '20190423');
-    wp_enqueue_style( 'prism' );
-
     wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,600,700', false );
   }
 }
 
 add_action('wp_enqueue_scripts', 'add_styles');
-
-?>
