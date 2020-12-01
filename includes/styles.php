0c6c92fa6fb9eb16a72393cfa83a40f9755de2d1
Guillaume Corré <gui.corre@gmail.com>
2020-12-01 11:30:42 +0100

    Bump versions

--- includes/styles.php
+++ includes/styles.php
@@ -2,16 +2,16 @@
 
 function add_styles() {
   if (!is_admin()) {
-    wp_register_style( 'main', get_template_directory_uri() .'/assets/css/main.min.css', array(), '20190423');
+    wp_register_style( 'main', get_template_directory_uri() .'/assets/css/main.min.css', array(), '20201201');
     wp_enqueue_style( 'main' );
 
-    wp_register_style( 'slick', get_template_directory_uri() .'/assets/css/vendor/slick.css', array(), '20190423');
+    wp_register_style( 'slick', get_template_directory_uri() .'/assets/css/vendor/slick.css', array(), '20201026');
     wp_enqueue_style( 'slick' );
 
-    wp_register_style( 'slick-theme', get_template_directory_uri() .'/assets/css/vendor/slick-theme.css', array(), '20190423');
+    wp_register_style( 'slick-theme', get_template_directory_uri() .'/assets/css/vendor/slick-theme.css', array(), '20201026');
     wp_enqueue_style( 'slick-theme' );
 
-    wp_register_style( 'fancybox', get_template_directory_uri() .'/assets/css/vendor/jquery.fancybox.css', array(), '20190423');
+    wp_register_style( 'fancybox', get_template_directory_uri() .'/assets/css/vendor/jquery.fancybox.css', array(), '20201026');
     wp_enqueue_style( 'fancybox' );
 
     wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700', false );
